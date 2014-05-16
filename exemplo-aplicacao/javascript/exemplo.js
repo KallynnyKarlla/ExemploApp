1fa9f8cdf890e2debeb2e08e9d1350295cef81b9
claudivanfilho <j.claudivan.filho@gmail.com>
2014-05-16 08:23:18 -0300

    merge

--- exemplo-aplicacao/javascript/exemplo.js
+++ exemplo-aplicacao/javascript/exemplo.js
@@ -6,19 +6,28 @@ function adicionaSerie() {
 
 	$id = $nome.split(" ").join("");  // tira os espaços em branco do nome
 
-	if ($('#' + $id).length) { // se ja existir uma div com esse id lança uma msm de erro
-		alert("Seriado Já existe!");
+	if ($('#' + $id).length) { // se já existir uma div com esse id lança uma msm de erro
+		alert("Seriado já existe!");
 	} else {
+
 		$('#series').append('<div class="label label-danger col-sm-5"  id="' + $id + '"> </div>');
 		$divSeriado = $('#' + $id);
 		$divSeriado.append("<h3>" + $nome);
 		$divSeriado.append("<br /> ");
 		$divSeriado.append("<strong> Descrição: </strong>" + $descricao);
 		$divSeriado.append("<br /> ");
-		$divSeriado.append("<strong> Lancamento: </strong>" + $lancamento);
+		$divSeriado.append("<strong> Lançamento: </strong>" + $lancamento);
 		$divSeriado.append("<br /> ");
+
+		limpaForms();
 		$divSeriado.hide().fadeIn();
 	}
+}
+
+function limpaForms() {
+	$('#nome').val('');
+	$('#descricao').val('');
+	$('#lancamento').val('');
 }
 
 function removerSerie(){
