8d64fc2ff8d1e5227ab2ba96647b1eb489ef172f
claudivanfilho <j.claudivan.filho@gmail.com>
2014-05-16 09:02:31 -0300

    exemplo sem o css adicionado

---
+++ exemplo-aplicacao-sem-CSS/javascript/exemplo.js
@@ -0,0 +1,36 @@
+function adicionaSerie() {
+	$nome = $('#nome').val();
+	$descricao = $('#descricao').val();
+	$lancamento = $('#lancamento').val();
+
+	$id = $nome.split(" ").join("");  // tira os espaços em branco do nome
+
+	if ($('#' + $id).length) { // se já existir uma div com esse id lança uma msg de erro
+		alert("Seriado já existe!");
+	} else {
+
+		$('#series').append('<div id="' + $id + '"> </div>');
+		$divSeriado = $('#' + $id);
+		$divSeriado.append("<h3>" + $nome);
+		$divSeriado.append("<br /> ");
+		$divSeriado.append("<strong> Descrição: </strong>" + $descricao);
+		$divSeriado.append("<br /> ");
+		$divSeriado.append("<strong> Lançamento: </strong>" + $lancamento);
+		$divSeriado.append("<br /> ");
+
+		limpaForms();
+		$divSeriado.hide().fadeIn();
+	}
+}
+
+function limpaForms() {
+	$('#nome').val('');
+	$('#descricao').val('');
+	$('#lancamento').val('');
+}
+
+function removerSerie(){
+	$nome = $('#nomeRem').val();
+	$id = $nome.split(" ").join("");  // tira os espaços em branco do nome
+	$('#'+ $id).fadeOut();	
+} 
