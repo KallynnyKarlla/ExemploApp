4e9e182f2d4e2481628f7bb093111e700243351b
claudivanfilho <j.claudivan.filho@gmail.com>
2014-05-16 08:32:32 -0300

    merge

--- exemplo-aplicacao/javascript/exemplo.js
+++ exemplo-aplicacao/javascript/exemplo.js
@@ -1,16 +1,11 @@
 function adicionaSerie() {
-
 	$nome = $('#nome').val();
 	$descricao = $('#descricao').val();
 	$lancamento = $('#lancamento').val();
 
 	$id = $nome.split(" ").join("");  // tira os espaços em branco do nome
 
-<<<<<<< HEAD:exemplo-aplicacao/javascript/exemplo.js
-	if ($('#' + $id).length) { // se já existir uma div com esse id lança uma msm de erro
-=======
 	if ($('#' + $id).length) { // se já existir uma div com esse id lança uma msg de erro
->>>>>>> 5516c6826e7aea923e1cb802828095dbaca9e5f9:exemplo-aplicacao/exemplo.js
 		alert("Seriado já existe!");
 	} else {
 
@@ -22,15 +17,9 @@ function adicionaSerie() {
 		$divSeriado.append("<br /> ");
 		$divSeriado.append("<strong> Lançamento: </strong>" + $lancamento);
 		$divSeriado.append("<br /> ");
-<<<<<<< HEAD:exemplo-aplicacao/javascript/exemplo.js
 
 		limpaForms();
 		$divSeriado.hide().fadeIn();
-=======
-		limpaForms();
-		alert('Seriado Adicionado!');
-		
->>>>>>> 5516c6826e7aea923e1cb802828095dbaca9e5f9:exemplo-aplicacao/exemplo.js
 	}
 }
 
@@ -38,14 +27,10 @@ function limpaForms() {
 	$('#nome').val('');
 	$('#descricao').val('');
 	$('#lancamento').val('');
-<<<<<<< HEAD:exemplo-aplicacao/javascript/exemplo.js
 }
 
 function removerSerie(){
 	$nome = $('#nomeRem').val();
 	$id = $nome.split(" ").join("");  // tira os espaços em branco do nome
 	$('#'+ $id).fadeOut();	
-}
-=======
 } 
->>>>>>> 5516c6826e7aea923e1cb802828095dbaca9e5f9:exemplo-aplicacao/exemplo.js
